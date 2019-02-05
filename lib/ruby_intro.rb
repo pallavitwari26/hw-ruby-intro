2425732ac39c7f3bde537db0d7f2563a82030d07
pallavitiwari26 <pallavitiwari26@gmail.com>
2019-02-05 22:35:44 +0000

    1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+ arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  case arr.length
+	when 0
+		return 0
+	when 1 
+		return arr.first
+	else
+		return arr.sort.last(2).sum
+  end
+
 end
 
+
+
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.permutation(2).any? {|a, b| (a + b) == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+ if s.empty?
+  	return false
+  elsif ( /[a-z]/ =~ s[0].downcase && /[^aeiou]/ =~ s[0].downcase)
+    return true
+  else
+    return false
+ end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s.to_s =~ /\b[01]+\b/ && s.to_i % 4 == 0)
+	return true
+	else
+		return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+ attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%.2f" % @price
+  end
 end
